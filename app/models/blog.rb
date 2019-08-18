960dfcde4c4000bff8e6a57462c1db60a36b02c0
Tejota <tejota317@gmail.com>
2019-08-18 10:48:19 -0400

    Implemented helper methos from ActionView

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,7 +6,7 @@ class Blog < ApplicationRecord
 
   validates_presence_of :title, :body
 
-  belongs_to :topic
+  belongs_to :topic, optional: true 
 
   #creating a default value
   after_initialize :set_defaults
